c75d0485594a066f2e1f81f37bca8c2bea637bcd
Anton Normelius <a.normelius@gmail.com>
2022-10-18 14:36:42 +0200

    Smaller modification and added tests for euler

--- approxer/odes/euler_lib.cc
+++ approxer/odes/euler_lib.cc
@@ -1,13 +1,13 @@
 
+/*
+euler_lib.cc contains code for running the forward euler algorithm.
+*/
+
 #include <iostream>
 #include <memory>
 
-
 typedef struct Arguments {
-    // User specified function.
     void (*ode)(double, double*, const unsigned long int, double*);
-    // shape(N, ), 1d array.
-    // User specified initial values with shape (nCols,)
     double *y;
     const unsigned long int iterations;
     const unsigned long int ncols;
@@ -20,8 +20,8 @@ typedef struct Arguments {
 void euler(Arguments *args) {
     // At every iterations, this is used to fetch the update from the
     // user specified ODE.
-    std::shared_ptr<double[]> yTempShared( new double[args->ncols]);
-    double *yTemp = yTempShared.get();
+    std::shared_ptr<double[]> yTempShared(new double[args->ncols]);
+    double *yIntermediateBuffer= yTempShared.get();
     const unsigned long int ncols = args->ncols;
     double t_0 = args->t_0;
     double *y = args->y;
@@ -31,19 +31,20 @@ void euler(Arguments *args) {
     void (*ode)(double, double*, const unsigned long int, double*) = args->ode;
     args->t_out[0] = t_0;
 
-    // Initialize the first values.
+    // Initialize the first values with the user specified values.
     for (int idx = 0; idx < ncols; ++idx) {
         y_out[idx] = y[idx];
     }
 
     const double step_size = (args->t - t_0) / (double) args->iterations;
     // Start iterating at 1 since we have default values for 0.
+
     for (unsigned long int i = 1; i <= args->iterations; i++) {
         t_0 += step_size;
         // Update yTempwith the user specified ode.
-        ode(t_0, y, ncols, yTemp);
+        ode(t_0, y, ncols, yIntermediateBuffer);
         for (int j = 0; j < ncols; j++) {
-            y[j] += step_size * yTemp[j];
+            y[j] += step_size * yIntermediateBuffer[j];
             y_out[i * (j + ncols)] = y[j];
         }
 
